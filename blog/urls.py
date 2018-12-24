f6a078830bcea42fb48427b8e76621ff96d4faac
everstalk <delasi208@gmail.com>
2018-12-24 09:25:01 +0000

    created edit_post template

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
@@ -7,7 +8,16 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/', views.post_list, name='post_list'),
-    path('post/<int:post_id>/', views.detail, name='detail'),
-    path('<int:post_id>/', views.results, name='results'),
-    path('new/', views.new_post, name='new post'),
+    path('post/<int:pk>/', views.detail, name='detail'),
+    path('<int:pk>/', views.results, name='results'),
+    path('new/', views.new_post, name='new_post'),
+    path('edit/', views.edit_post, name='edit_post'),
 ]
+"""
+urlpatterns = [
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^post/new/$', views.new_post, name='new_post'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.edit_post, name='edit_post'),
+]
+"""
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, get_object_or_404
+import datetime
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
 from django.template import loader 
+from django.utils import timezone
+
 
 from .models import Post
 from .forms import PostForm
 
-# Create your views here.
+
 def index(request):
     return render(request,'blog/index.html')
 
@@ -15,16 +18,46 @@ def post_list(request):
         'latest_post_list': latest_post_list
     }
     return render(request, 'blog/post_list.html', context)
-
-def detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
+"""
+def get_post_data(request):
+    if request.method == 'POST':
+        form=PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return render(request, 'blog/post_list.html')
+"""
+def detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/detail.html', {'post': post})
 
-def results(request, post_id):
+def results(request, pk):
     response = "You're looking at the results of Post %s."
-    return HttpResponse(response % post_id)
+    return HttpResponse(response % pk)
 
 def new_post(request):
-    form = PostForm()
-    return render(request, 'blog/new_post.html', {'form': form})
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/edit_post.html', {'form': form})
+
+def edit_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/edit_post.html', {'form': form})
 
